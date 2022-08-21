4888a2173c764f8bc54307fa9ee4fba0a2cc3325
Marvin Mager <99667992+mvnmgrx@users.noreply.github.com>
2022-08-21 18:44:57 +0200

    Module: Added setup and init files

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup script for kiutils
+
+Authors:
+    (C) Marvin Mager - @mvnmgrx - 2022
+
+License identifier:
+    GPL-3.0
+"""
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup()

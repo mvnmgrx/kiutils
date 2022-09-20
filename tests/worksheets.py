bca875e51d7e0ddaed30f967648fb4edae20fb08
Marvin Mager <99667992+mvnmgrx@users.noreply.github.com>
2022-09-20 22:20:16 +0200

    Tests: Added empty worksheet test case

--- tests/worksheets.py
+++ tests/worksheets.py
@@ -29,6 +29,13 @@ class Tests_WorkSheets(unittest.TestCase):
         wks = WorkSheet().from_file(self.testData.pathToTestFile)
         self.assertTrue(to_file_and_compare(wks, self.testData))
 
+    def test_createEmptyWorksheet(self):
+        """Tests the parsing of all available worksheet items"""
+        self.testData.compareToTestFile = True
+        self.testData.pathToTestFile = path.join(WORKSHEET_BASE, 'test_createEmptyWorksheet')
+        wks = WorkSheet()
+        self.assertTrue(to_file_and_compare(wks, self.testData))
+
     def tearDown(self) -> None:
         cleanup_after_test(self.testData)
         return super().tearDown()
